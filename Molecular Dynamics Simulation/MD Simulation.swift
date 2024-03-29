41f28bb4710de8b3008bf757b41dab835be6f7a3
Zachary Ives <zives@hawk.iit.edu>
2024-03-29 18:49:12 -0500

    Translated example to swift fully

--- Molecular Dynamics Simulation/MD Simulation.swift
+++ Molecular Dynamics Simulation/MD Simulation.swift
@@ -10,7 +10,7 @@ import Observation
 
 @Observable class MDSimulation{
     var L: Int = 1
-    var numAtoms: Int = 8
+    var nAtom: Int = 8
     var nMax: Int = 513 //rename this once you figure out what it does
     var x: [Double] = []
     var fx: [[Double]] = [[]]
@@ -18,6 +18,75 @@ import Observation
     init() {
         self.x = Array(repeating: 0, count: self.nMax)
         self.fx = Array(repeating: Array(repeating: 0, count: nMax), count: 2)
+    }
+    
+    func runSimulation() {
+        var t1: Int
+        var t2: Int
+        var i: Int
+        var iTemp: Int
+        var t: Int
+        var nStep: Int = 5000
+        var nPrint: Int = 100
+        var nDim: Int = 1
+        
+        var h: Double = 0.0004
+        var hover2: Double
+        var PE: Double
+        var KE: Double
+        var T: Double
+        var tInit: Double = 10.0
+        var vx: [Double] = Array(repeating: 0, count: nMax)
+        
+        self.L = Int(pow(Double(nAtom), 1.0/Double(nDim)))
+        self.nAtom = Int(pow(Double(L),Double(nDim)))
+        
+        print("nAtom = \(nAtom) L = \(L)")
+        i = -1
+        for ix in stride(from: 0, through: L-1, by: 1){
+            i = i+1
+            x[i] = Double(ix)
+            vx[i] = (Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0)+Double.random(in: 0.0 ... 1.0))/12.0 - 0.5
+            vx[i] = vx[i]*sqrt(tInit)
+            print("init vx = \(vx[i])")
+        }
+        t1 = 0
+        t2 = 1
+        hover2 = h/2.0
+        t = 0
+        KE = 0.0
+        PE = 0.0
+        PE = forces(t: t1, PE: PE)
+        for i in stride(from: 0, through: nAtom - 1, by: 1) {
+            KE = KE + (vx[i]*vx[i])/2
+        }
+        print("\(t) PE = \(PE) KE = \(KE) PE+KE = \(PE+KE)")
+        for t in stride(from: 1, to: nStep, by: 1){
+            for i in stride(from: 0, through: nAtom - 1, by: 1) {
+                PE = forces(t: t1, PE: PE)
+                x[i] = x[i] + h*(vx[i] + hover2*fx[i][t1])
+                if (x[i] <= 0.0){
+                    x[i] = x[i] + Double(L)
+                }
+                if (x[i] >= Double(L)){
+                    x[i] = x[i] - Double(L)
+                }
+            }
+            PE = forces(t: t2, PE: PE)
+            KE = 0
+            for i in stride(from: 0, through: nAtom - 1, by: 1){
+                vx[i] = vx[i] + hover2*(fx[i][t1] + fx[i][t2])
+                KE = KE + (vx[i]*vx[i])/2
+            }
+            T = 2.0 * KE / (3.0 * Double(nAtom))
+            if (t % nPrint == 0){
+                print("\(t) PE = \(PE) KE = \(KE) PE+KE = \(PE+KE)")
+            }
+            iTemp = t1
+            t1 = t2
+            t2 = iTemp
+        }
+                
     }
     
     func forces(t: Int, PE: Double) -> Double {
@@ -28,12 +97,12 @@ import Observation
         var r2cut: Double = 9.0
         
         var newPE: Double = 0.0
-        for i in stride(from: 0, to: numAtoms - 1, by: 1){
+        for i in stride(from: 0, to: nAtom - 1, by: 1){
             fx[i][t] = 0
         }
         
-        for i in stride(from: 0, through: numAtoms - 2, by: 1){
-            for j in stride(from: i+1, through: numAtoms - 1, by: 1) {
+        for i in stride(from: 0, through: nAtom - 2, by: 1){
+            for j in stride(from: i+1, through: nAtom - 1, by: 1) {
                 dx = x[i] - x[j]
                 if (abs(dx) > 0.5*Double(L)){
                     dx = dx - sign(a: Double(L), b: dx)
