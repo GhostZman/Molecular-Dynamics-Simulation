7c43b8b2d640e9088770acebca347fd9b11093bc
Zachary Ives <zives@hawk.iit.edu>
2024-03-29 16:56:30 -0500

    Initial Commit

---
+++ Molecular Dynamics Simulation/Molecular_Dynamics_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Molecular_Dynamics_SimulationApp.swift
+//  Molecular Dynamics Simulation
+//
+//  Created by Phys440Zachary on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Molecular_Dynamics_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
